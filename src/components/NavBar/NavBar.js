74936b0fd8c7beaa298e492e6ad46f5d159ddc1e
anamorales-02 <akmorales.02@gmail.com>
2023-02-16 21:56:17 -0300

    Ultimos detalles

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,15 +9,16 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        AnBikinis
+        <h1>AnBikinis</h1>
         <button
+         id="toggleButton"
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
-          aria-label="Toggle navigation"
+          aria-label="Mostrar menú de navegación"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
